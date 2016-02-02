10a519ed6b8539a4c34918ecfd94c5878a52c6ba
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-02-02 21:44:38 +0200

    Make class Part and update class Car

--- CarPartsShopGit/CarPartsShopGit/Car.cs
+++ CarPartsShopGit/CarPartsShopGit/Car.cs
@@ -22,5 +22,74 @@ namespace CarPartsShopGit
         {
             return "<" + this.brand + "," + this.model + "," + this.productionYear + ">";
         }
+        public override int GetHashCode()
+        {
+
+            const int prime = 31;
+            int result = 1;
+            result = prime * result + ((this.brand == null) ? 0 :
+            this.brand.GetHashCode());
+            result = prime * result + ((this.model == null) ? 0 :
+            this.model.GetHashCode());
+            result = prime * result + ((this.productionYear == null) ? 0 :
+            this.productionYear.GetHashCode());
+            return result;
+        }
+        public override bool Equals(Object obj)
+        {
+            if (this == obj)
+            {
+                return true;
+            }
+
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (this.GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Car other = (Car)obj;
+            if (this.brand == null)
+            {
+                if (other.brand != null)
+                {
+                    return false;
+                }
+            }
+            else if (!this.brand.Equals(other.brand))
+            {
+                return false;
+            }
+
+            if (this.model == null)
+            {
+                if (other.model != null)
+                {
+                    return false;
+                }
+            }
+            else if (!this.model.Equals(other.model))
+            {
+                return false;
+            }
+
+            if (this.productionYear == null)
+            {
+                if (other.productionYear != null)
+                {
+                    return false;
+                }
+            }
+            else if (!this.productionYear.Equals(other.productionYear))
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
---
+++ CarPartsShopGit/CarPartsShopGit/Part.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarPartsShopGit
+{
+    class Part
+    {
+        public enum PartCategory
+        {
+            Engine,
+            Tires,
+            Exhaust,
+            Suspension,
+            Brakes
+        }
+
+        private String name;
+        private String code;
+        private PartCategory category;
+        private HashSet<Car> supportedCars;
+        private double buyPrice;
+        private double sellPrice;
+        private Manufacturer manufacturer;
+
+        public Part (string name, double buyPrice, double sellPrice,
+            Manufacturer manufacturer, string code, PartCategory category)
+        {
+            this.name = name;
+            this.buyPrice = buyPrice;
+            this.sellPrice = sellPrice;
+            this.manufacturer = manufacturer;
+            this.code = code;
+            this.category = category;
+            this.supportedCars = new HashSet<Car>();
+        }
+        
+        public void AddSupportedCar(Car car)
+        {
+            this.supportedCars.Add(car);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append("Part: " + this.name + "\n");
+            result.Append("-code: " + this.code + "\n");
+            result.Append("-category: " + this.category + "\n");
+            result.Append("-buyPrice: " + this.buyPrice + "\n");
+            result.Append("-sellPrice: " + this.sellPrice + "\n");
+            result.Append("-manufacturer: " +
+            this.manufacturer + "\n");
+            result.Append("---Supported cars---" + "\n");
+            foreach (Car car in this.supportedCars)
+            {
+                result.Append(car);
+                result.Append("\n");
+            }
+            result.Append("----------------------\n");
+            return result.ToString();
+        }
+       
+        
+
+    }
+}
