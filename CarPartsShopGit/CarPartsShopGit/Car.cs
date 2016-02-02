d65a75b4f834e5654821952f067aa6cc7aca1532
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2016-02-02 18:35:02 +0200

    Make Class Car

---
+++ CarPartsShopGit/CarPartsShopGit/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarPartsShopGit
+{
+    class Car
+    {
+        private string brand;
+        private string model;
+        private string productionYear;
+
+        public Car(string brand, string model, string productionYear)
+        {
+            this.brand = brand;
+            this.model = model;
+            this.productionYear = productionYear;
+        }
+        public override string ToString()
+        {
+            return "<" + this.brand + "," + this.model + "," + this.productionYear + ">";
+        }
+    }
+}
